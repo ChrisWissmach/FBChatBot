3a793206d1a2e092fcfa816d67c8c410b9846516
Chris Wissmach <chriswissmach@gmail.com>
2017-10-09 17:31:29 +0000

    Fix indentation

--- helpers.py
+++ helpers.py
@@ -12,15 +12,15 @@ parameter `i` used to obtain the correct image of the chart.
 def parse_ticker(msg):
 	valid_ranges = ['d', 'm', 'y']
 	ranges = {
-    '1d': 60,
-    '5d': 240,
-    '1M': 86400,
-    '3M': 86400,
-    '6M': 86400,
-    '1Y': 86400,
-    '5Y': 604800,
-    '40Y': 604800,
-  }
+	    '1d': 60,
+	    '5d': 240,
+	    '1M': 86400,
+	    '3M': 86400,
+	    '6M': 86400,
+	    '1Y': 86400,
+	    '5Y': 604800,
+	    '40Y': 604800,
+  	}
 	after_dollar = (msg.split("$")[1]).split(" ")[:2]
 	ticker = re.split('[^a-zA-Z\.\-]', after_dollar[0])[0].upper()
 	# if the ticker is entered as a dollar amount
@@ -33,10 +33,10 @@ def parse_ticker(msg):
 		if after_dollar[1] == "max":
 			duration = "40Y"
 		else:
-	  	if after_dollar[1][0].isnumeric() and (after_dollar[1][1].lower() in valid_ranges):
-	    	duration = after_dollar[1][0] + after_dollar[1][1]
-	    	if after_dollar[1][1].lower() != 'd':
-	    		duration = after_dollar[1][0] + after_dollar[1][1].upper()
+			if after_dollar[1][0].isnumeric() and (after_dollar[1][1].lower() in valid_ranges):
+				duration = after_dollar[1][0] + after_dollar[1][1]
+			if after_dollar[1][1].lower() != 'd':
+				duration = after_dollar[1][0] + after_dollar[1][1].upper()
 	i = 60
 	if duration in ranges:
 		i = ranges[duration]
